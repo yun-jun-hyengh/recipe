03121fcfc556ed710687d21cc1ff2d8e3b847e9a
yun-jun-hyengh <77119415+yun-jun-hyengh@users.noreply.github.com>
2025-09-02 12:51:18 +0900

    2025-09-02

---
+++ src/main/java/babmukja/system/recipe/entity/Customer.java
@@ -0,0 +1,55 @@
+package babmukja.system.recipe.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @Column(name = "user_idx")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_idx;
+
+    @Column(name = "user_id")
+    private String user_id;
+
+    @Column(name = "user_pw")
+    private String user_pw;
+
+    @Column(name = "user_name")
+    private String user_name;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "user_email")
+    private String user_email;
+
+    @Column(name = "user_phone")
+    private String user_phone;
+
+    @Column(name = "adminchk")
+    private Long adminchk;
+
+    @Column(name = "regdate")
+    private String regdate;
+
+    @Column(name = "private_recipe_limit", columnDefinition = "INT DEFAULT 50")
+    private Long private_recipe_limit;
+
+    @Column(name = "unlimit", columnDefinition = "INT DEFAULT 0")
+    private Long unlimit; // 0 : 제한   1 : 제한없음
+
+    
+}
