86137adb8386785e2372e3faad1dcf9eba1f2b44
yun-jun-hyengh <77119415+yun-jun-hyengh@users.noreply.github.com>
2025-09-08 23:43:45 +0900

    2025-09-08

--- frontend/src/pages/NoticeWritePage.tsx
+++ frontend/src/pages/NoticeWritePage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { noticeApi } from '../api/noticeApi';
+import { NoticeRequest } from '../types/notice';
 
 const NoticeWritePage = () => {
     const navigate = useNavigate();
