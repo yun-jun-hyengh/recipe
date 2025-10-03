10446566b4abcc76f1febefbf9171e22e9e45fb4
yun-jun-hyengh <77119415+yun-jun-hyengh@users.noreply.github.com>
2025-10-03 18:22:16 +0900

    2025-10-03

--- frontend/src/pages/PopupListPage.tsx
+++ frontend/src/pages/PopupListPage.tsx
@@ -12,9 +12,27 @@ const PopupListPage = () => {
                     <div className="flex justify-between items-center mb-4">
                         <h3 className="font-bold mb-2">팝업 리스트</h3>
                         <div className="flex items-center space-x-2">
-
+                            <button
+                                className="px-4 py-2 bg-purple-800 text-white font-semibold rounded-md hover:bg-purple-900 transition">
+                                팝업등록
+                            </button>
                         </div>
                     </div>
+                    <table className='w-full text-sm text-center'>
+                        <thead>
+                            <tr className="text-left text-gray-400">
+                                <th className='text-center'>번호</th>
+                                <th className='text-center'>팝업 제목</th>
+                                <th className='text-center'>배너 설명</th>
+                                <th className='text-center'>시작일</th>
+                                <th className='text-center'>종료일</th>
+                                <th className='text-center'>배너 상태</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
                 </div>
             </main>
         </div>
