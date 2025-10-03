085a5436a73a0db7954d621553dc2b021d934043
yun-jun-hyengh <77119415+yun-jun-hyengh@users.noreply.github.com>
2025-10-04 04:35:23 +0900

    2025-10-04

---
+++ src/main/java/babmukja/system/recipe/entity/Popup.java
@@ -0,0 +1,38 @@
+package babmukja.system.recipe.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "popup")
+public class Popup {
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "start_date")
+    private String start_date;
+
+    @Column(name = "end_date")
+    private String end_date;
+
+    @Column(name = "is_active")
+    private Long is_active;
+}
