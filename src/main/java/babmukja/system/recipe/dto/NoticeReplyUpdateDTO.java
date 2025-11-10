67692af048c25c0bc0c8ea45713ace917eacbb89
yun-jun-hyengh <77119415+yun-jun-hyengh@users.noreply.github.com>
2025-11-10 17:49:10 +0900

    2025-11-10

--- src/main/java/babmukja/system/recipe/dto/NoticeReplyUpdateDTO.java
+++ src/main/java/babmukja/system/recipe/dto/NoticeReplyUpdateDTO.java
@@ -1,5 +1,10 @@
 package babmukja.system.recipe.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class NoticeReplyUpdateDTO {
     private Long re_idx;
     private Long user_idx;
